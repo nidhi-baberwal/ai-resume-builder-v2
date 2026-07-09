89a21e72c65153108d04e4f41c47ce07200345a2
nidhi-baberwal <nidhibagoria29@gmail.com>
2026-07-09 15:31:09 +0530

    feat: add Experience Form with live resume preview

--- src/pages/ResumeEditor.tsx
+++ src/pages/ResumeEditor.tsx
@@ -4,6 +4,7 @@ import type { Resume } from "../types/resume";
 import PersonalInfoForm from "../resume/PersonalInfoForm";
 import SummaryForm from "../resume/SummaryForm";
 import EducationForm from "../resume/EducationForm";
+import ExperienceForm from "../resume/ExperienceForm";
 
 export default function ResumeEditor(){
 
@@ -44,6 +45,10 @@ export default function ResumeEditor(){
         setResume = {setResume}
         />
 
+        <ExperienceForm
+        resume = {resume}
+        setResume = {setResume}
+        />
     </div>
 
     <div className="toolbar">
@@ -75,8 +80,6 @@ export default function ResumeEditor(){
 
         <h2>Education</h2>
 
-        <h2>Education</h2>
-
         {resume.education.map((edu, index) => (
         <div key={index}>
         <h4>{edu.degree}</h4>
@@ -89,12 +92,18 @@ export default function ResumeEditor(){
        </div>
         ))}
 
-        <h3>Skills</h3>
-        <ul>
-            <li>React</li>
-            <li>Typescript</li>
-            <li>Javascript</li>
-        </ul>
+       <h2>Experience</h2>
+
+       {resume.experience.map((exp, index) => (
+        <div key={index}>
+          <h4>{exp.company}</h4>
+          <p>{exp.position}</p>
+          <p>
+            {exp.startDate} - {exp.endDate}
+            </p>
+            <p>{exp.description}</p>
+        </div>
+       ))}
     </div>
 </div>
 
---
+++ src/resume/ExperienceForm.tsx
@@ -0,0 +1,102 @@
+import type { Resume } from "../types/resume";
+
+interface ExperienceFormProps{
+ resume: Resume;
+ setResume: React.Dispatch<React.SetStateAction<Resume>>
+}
+
+export default function ExperienceForm({
+   resume,
+   setResume,
+}: ExperienceFormProps) {
+
+    const addExperience = () => {
+        setResume({
+            ...resume,
+            experience: [
+                ...resume.experience,
+            {
+                company: "",
+                position: "",
+                startDate: "",
+                endDate: "",
+                description: ""
+            },
+            ],
+        });
+    }
+
+    const handleChange = (
+        index: number,
+        field: string,
+        value: string,
+    ) => {
+
+        const updatedExperience = [...resume.experience];
+
+        updatedExperience[index] = {
+            ...updatedExperience[index],
+            [field]: value,
+        };
+
+        setResume({
+            ...resume,
+            experience: updatedExperience,
+        });
+    }
+    return(
+        <>
+         <h2>Experience</h2>
+
+         {resume.experience.map((exp, index) => (
+            <div key = {index}>
+                <input
+                type="text"
+                placeholder="Company"
+                value={exp.company}
+                onChange={(e) =>
+                    handleChange(index, "company", e.target.value)}
+                />
+
+                <input
+                type="text"
+                placeholder="Position"
+                value={exp.position}
+                onChange={(e) =>
+                    handleChange(index, "position", e.target.value)}
+                />
+
+                <input
+                type="text"
+                placeholder="Start Date"
+                value={exp.startDate}
+                onChange={(e) =>
+                    handleChange(index, "startDate", e.target.value)}
+                />
+
+                <input
+                type="text"
+                placeholder="End Date"
+                value={exp.endDate}
+                onChange={(e) =>
+                    handleChange(index, "endDate", e.target.value)}
+                />
+
+                <input
+                type="text"
+                placeholder="Description"
+                value={exp.description}
+                onChange={(e) =>
+                    handleChange(index, "description", e.target.value)}
+                />
+
+            </div>
+         ))}
+
+         <button onClick = {addExperience}>
+            Add Experience
+         </button>
+        </>
+
+    );
+}
